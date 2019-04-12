d63cc008247c792c1195edfc18b0b8732bd1dd39
谢永诚 <752106133@qq.com>
2019-04-12 13:39:56 +0800

    初始化解决方案

--- RC/main.cpp
+++ RC/main.cpp
@@ -1,8 +1,9 @@
+#include <bits/stdc++.h>
 #include <Windows.h>
 
 using namespace std;
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR pCmdLine, int nShowCmd) {
+	MessageBox(NULL, TEXT("³É¹¦"), TEXT(""), MB_OK);
 	return 0;
 }
